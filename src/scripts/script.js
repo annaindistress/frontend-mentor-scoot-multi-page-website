dc332d3648408df41dd1dd131418143ce980fdd8
Anna Artemova <annartmva@gmail.com>
2023-03-04 20:19:48 +0400

    Add section with page title

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,17 +1,22 @@
 'use strict';
 
+const headerElement = document.querySelector('.header');
 const headerToggleElement = document.querySelector('.header__menu-button');
 const navigationElement = document.querySelector('.navigation');
 
 class App {
-  constructor () {
-    headerToggleElement.addEventListener('click', this._toggleMobileMenu.bind(this));
+  constructor() {
+    headerToggleElement.addEventListener(
+      'click',
+      this._toggleMobileMenu.bind(this)
+    );
   }
 
   _toggleMobileMenu() {
     headerToggleElement.classList.toggle('header__menu-button--open');
     navigationElement.classList.toggle('navigation--open');
+    headerElement.classList.toggle('header--open');
   }
-};
+}
 
 const app = new App();
